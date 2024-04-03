be467341f609c1f6dc5c017220f5d7ac2262d447
Yuuya-s <salorkyuuya@gmail.com>
2024-04-03 05:25:10 +0300

    commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    """Print the first x integers of a list.
+
+    Args:
+        my_list (list): The list to print integers from.
+        x (int): The number of elements of my_list to access.
+
+    Returns:
+        The real number of integers printed.
+    """
+    count = 0  
 
     try:
-        for i in range(x):
+        for i in range(x):  
             try:
-                print("{:d}".format(my_list[i]), end='')
-                count += 1
-            except (TypeError, ValueError):
-                pass
-    except IndexError:
-        pass
+                print("{:d}".format(my_list[i]), end='')  
+                count += 1  
+            except (TypeError, ValueError):  
+                pass  
+    except IndexError:  
+        pass  
 
-    print()
-    return count
+    print()  
+    return count  
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    """Returns the division of a by b."""
+    """Divides two integers and prints the result.
+
+    Args:
+        a (int): The numerator.
+        b (int): The denominator.
+
+    Returns:
+        The result of the division, or None if an error occurs.
+    """
     try:
         div = a / b
     except (TypeError, ZeroDivisionError):
         div = None
     finally:
         print("Inside result: {}".format(div))
-    return (div)
+        return div
